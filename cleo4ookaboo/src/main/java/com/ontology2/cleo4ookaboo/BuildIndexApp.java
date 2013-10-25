d5ab04ff913f1910a69764ba45864a10d6fe570f
Paul Houle <paul@ontology2.com>
2013-10-25 16:52:19 -0400

    on windows we're running something that gets the relevant data out in a few seconds

--- cleo4ookaboo/src/main/java/com/ontology2/cleo4ookaboo/BuildIndexApp.java
+++ cleo4ookaboo/src/main/java/com/ontology2/cleo4ookaboo/BuildIndexApp.java
@@ -1,5 +1,9 @@
 package com.ontology2.cleo4ookaboo;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.jdbc.support.rowset.SqlRowSet;
 import org.springframework.stereotype.Component;
 
 import com.ontology2.centipede.shell.CommandLineApplication;
@@ -7,9 +11,30 @@ import com.ontology2.centipede.shell.CommandLineApplication;
 @Component
 public class BuildIndexApp extends CommandLineApplication {
 
+	@Autowired DriverManagerDataSource dataSource;
+	
 	@Override
 	protected void _run(String[] arguments) throws Exception {
-		System.out.println("foo");
+		JdbcTemplate t=new JdbcTemplate(dataSource);
+		SqlRowSet row=t.queryForRowSet( 
+				"SELECT topic.id,title,slug,quality_score_2,name"
+				+" FROM topic,topic_alt_name"
+				+" WHERE workflow_status>600"
+				+" AND topic.id=topic_alt_name.id"
+				);
+		
+		int cnt=0;
+		while(!row.isAfterLast()) {
+			cnt++;
+			if (cnt%1000==0) {
+				System.out.print(cnt);
+				System.out.print(' ');
+				System.out.println(row.getString(2));
+				}
+			row.next();
+		}
+		
+		System.out.println(cnt);
 	}
 
 }
